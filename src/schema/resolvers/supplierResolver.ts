243505f809605b73ff00047d74aa5f9c8ecc00cc
Jelica Stanojevic <jelicastanojevicc@gmail.com>
2019-05-26 20:09:39 +0200

    added transactions to resolvers

--- src/schema/resolvers/supplierResolver.ts
+++ src/schema/resolvers/supplierResolver.ts
@@ -2,37 +2,63 @@ import { IResolverObject } from 'graphql-tools';
 import { City } from '../../entity/City';
 import { Supplier } from '../../entity/Supplier';
 import { Partner } from '../../entity/Partner';
+import { getManager, ConnectionManager } from 'typeorm';
 
 export const resolvers: IResolverObject = {
-	Supplier: {
-		name: (root) => {
-			console.log(root);
-			return root.partner.name;
-		},
-		address: (root) => root.partner.address,
-		city: (root) => root.partner.city
-	},
-	Query: {
-		getCities: () => City.find(),
-		getSuppliers: () => Supplier.find({ relations: [ 'partner', 'partner.city' ] }),
-		getSupplier: (_, { taxIdNum }) => Supplier.findOne(taxIdNum)
-	},
-	Mutation: {
-		addSupplier: async (_, { taxIdNum, regNum, name, address, city }) => {
-			const newCity = await City.findOne(city);
-			const newPartner = await Partner.create({ taxIdNum, name, address, city: newCity }).save();
-			return await Supplier.create({ taxIdNum, regNum, partner: newPartner }).save();
-		},
-		updateSupplier: async (_, { taxIdNum, regNum, name, address, city }) => {
-			const newCity = await City.findOne(city);
-			await Partner.update(taxIdNum, { name, address, city: newCity });
-			await Supplier.update(taxIdNum, { regNum });
-			return await Supplier.findOne(taxIdNum);
-		},
-		deleteSupplier: async (_, { taxIdNum }) => {
-			await Supplier.delete(taxIdNum);
-			await Partner.delete(taxIdNum);
-			return true;
-		}
-	}
+  Supplier: {
+    name: (root) => {
+      console.log(root);
+      return root.partner.name;
+    },
+    address: (root) => root.partner.address,
+    city: (root) => root.partner.city,
+  },
+  Query: {
+    getCities: () => City.find(),
+    getSuppliers: () => Supplier.find({ relations: ['partner', 'partner.city'] }),
+    getSupplier: (_, { taxIdNum }) => Supplier.findOne(taxIdNum),
+  },
+  Mutation: {
+    addSupplier: async (_, { taxIdNum, regNum, name, address, city }) => {
+      await getManager().transaction(async (transactionalEntityManager) => {
+        const newCity = await City.findOne(city);
+        const newPartner = await transactionalEntityManager.insert(Partner, {
+          taxIdNum,
+          name,
+          address,
+          city: newCity,
+        });
+        const partner = await Partner.findOne(taxIdNum);
+        const newSupplier = await transactionalEntityManager.insert(Supplier, {
+          taxIdNum,
+          regNum,
+          partner,
+        });
+      });
+      return Supplier.findOne(taxIdNum, { relations: ['partner', 'partner.city'] });
+    },
+    updateSupplier: async (_, { taxIdNum, regNum, name, address, city }) => {
+      await getManager().transaction(async (transactionalEntityManager) => {
+        const newCity = await City.findOne(city);
+        await transactionalEntityManager.update(Partner, taxIdNum, {
+          name,
+          address,
+          city: newCity,
+        });
+        await transactionalEntityManager.update(Supplier, taxIdNum, { regNum });
+      });
+      return Supplier.findOne(taxIdNum, { relations: ['partner', 'partner.city'] });
+    },
+    deleteSupplier: async (_, { taxIdNum }) => {
+      return getManager().transaction(async (transactionalEntityManager) => {
+        console.log('here');
+        const supplierStatus = await transactionalEntityManager.delete(Supplier, taxIdNum);
+        if (supplierStatus.affected === 0) {
+          return false;
+        }
+        const partnerStatus = await transactionalEntityManager.delete(Partner, taxIdNum);
+        return partnerStatus.affected !== 0;
+      });
+    },
+  },
 };
--- src/schema/typeDefs/supplierType.ts
+++ src/schema/typeDefs/supplierType.ts
@@ -1,28 +1,40 @@
 import { gql } from 'apollo-server';
 
 export const typeDefs = gql`
-	type City {
-		areaCode: String!
-		name: String!
-	}
+  type City {
+    areaCode: String!
+    name: String!
+  }
 
-	type Supplier {
-		taxIdNum: String!
-		name: String!
-		address: String!
-		city: City!
-		regNum: String!
-	}
+  type Supplier {
+    taxIdNum: String!
+    name: String!
+    address: String!
+    city: City!
+    regNum: String!
+  }
 
-	extend type Query {
-		getCities: [City]
-		getSuppliers: [Supplier]
-		getSupplier(taxIdNum: String!): Supplier
-	}
+  extend type Query {
+    getCities: [City]
+    getSuppliers: [Supplier]
+    getSupplier(taxIdNum: String!): Supplier
+  }
 
-	extend type Mutation {
-		addSupplier(taxIdNum: String!, regNum: String!, name: String!, address: String!, city: String!): Supplier
-		updateSupplier(taxIdNum: String!, regNum: String!, name: String!, address: String!, city: String!): Supplier
-		deleteSupplier(taxIdNum: String!): Boolean
-	}
+  extend type Mutation {
+    addSupplier(
+      taxIdNum: String
+      regNum: String
+      name: String
+      address: String
+      city: String
+    ): Supplier
+    updateSupplier(
+      taxIdNum: String
+      regNum: String
+      name: String
+      address: String
+      city: String
+    ): Supplier
+    deleteSupplier(taxIdNum: String): Boolean
+  }
 `;
